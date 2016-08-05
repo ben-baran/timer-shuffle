378a5a423ccd7ef559bb7d50b0041b46bc4f6625
Ben Barantschik <bbarantschik@gmail.com>
2016-08-04 21:11:51 -0700

    Added charts.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-require('electron').webFrame.setZoomLevelLimits(1, 1)
+document.addEventListener("DOMContentLoaded", function(event)
+{
+	require('electron').webFrame.setZoomLevelLimits(1, 1)
+
+	var ctx = document.getElementById("myChart");
+	require('chart.js')
+	
+	var myChart = new Chart(ctx,
+	{
+    	type: 'doughnut',
+    	data:
+		{
+    		labels: ["Completed", "Not Completed"],
+    		datasets:
+			[
+        		{
+            		data: [300, 500],
+            		backgroundColor:
+					[
+               			"#FF6384",
+                		"#36A2EB"
+            		],
+            		hoverBackgroundColor:
+					[
+                		"#63FF84",
+                		"#36A2EB"
+            		]
+        		}
+			]
+		},
+    	options:
+		{
+        	legend:
+			{
+				display: false 
+        	},
+			tooltips:
+			{
+				enabled: false
+			}
+    	}
+	});
+})
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let mainWindow
 
 function createWindow()
 {
-	mainWindow = new BrowserWindow({width: 200, height: 600, frame: false, alwaysOnTop: true, resizable: false, backgroundColor: '#111'})
+	mainWindow = new BrowserWindow({width: 300, height: 600, frame: false, alwaysOnTop: true, resizable: false, backgroundColor: '#111'})
 	mainWindow.loadURL(`file://${__dirname}/index.html`)
 	mainWindow.on('closed', function(){ mainWindow = null})
 }
