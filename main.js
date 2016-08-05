b6d5a10f89cccdac27e813ff27acbfbd6a447673
Ben Barantschik <bbarantschik@gmail.com>
2016-08-04 19:23:24 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow()
+{
+	mainWindow = new BrowserWindow({width: 200, height: 600, frame: false, alwaysOnTop: true, resizable: false, backgroundColor: '#111'})
+	mainWindow.loadURL(`file://${__dirname}/index.html`)
+	mainWindow.on('closed', function(){ mainWindow = null})
+}
+
+app.on('ready', createWindow)
