608e5916a37b30b7917cc4babd0564853797f601
Ben B <bbarantschik@gmail.com>
2016-08-04 23:46:09 -0700

    Visual changes
    Timers still don’t work, but now title sticks to top and sections are
    laid out with charts (which currently display nothing)

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
 require('electron').webFrame.setZoomLevelLimits(1, 1);
+require('chart.js')
 $ = require('jquery');
 
 $(function()
 {
-	var ctx = $("#myChart");
-	require('chart.js');
-	
-	var myChart = new Chart(ctx,
-	{
-    	type: 'doughnut',
-    	data:
-		{
-    		labels: ["Completed", "Not Completed"],
-    		datasets:
-			[
-        		{
-            		data: [300, 500],
-            		backgroundColor:
-					[
-               			"#FF6384",
-                		"#36A2EB"
-            		],
-            		hoverBackgroundColor:
-					[
-                		"#63FF84",
-                		"#36A2EB"
-            		]
-        		}
-			]
-		},
-    	options:
-		{
-        	legend:
-			{
-				display: false 
-        	},
-			tooltips:
-			{
-				enabled: false
-			},
-			responsive: false
-		}
-	});
-
 	$('#addClock').on('click', function()
 	{
-		$('#clocks').append('<p style="color:#fff">Clicked the button</p>');
+		$.get('section.html', function(data)
+		{
+			$('#clocks').append(data);
+
+			var ctx = $("#clocks").last().first();
+			var newChart = new Chart(ctx,
+			{
+				type: 'doughnut',
+				data:
+				{
+					labels: ["Completed", "Not Completed"],
+					datasets:
+					[
+						{
+							data: [300, 500],
+							backgroundColor:
+							[
+								"#FF6384",
+								"#36A2EB"
+							],
+							hoverBackgroundColor:
+							[
+								"#63FF84",
+								"#36A2EB"
+							]
+						}
+					]
+				},
+				options:
+				{
+					legend:
+					{
+						display: false 
+					},
+					tooltips:
+					{
+						enabled: false
+					},
+					responsive: false
+				}
+			});
+
+		});
 	});
 });
